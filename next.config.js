1efa9f5a39b922f78322dc335acdd48f3fd5be9f
nakh76 <nakh76@gmail.com>
2022-09-23 17:07:50 +0900

    [Add] PWA 작업중

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+    disable: process.env.NODE_ENV === "development",
+    dest: "public",
+    register: true,
+    skipWaiting: false,
+    runtimeCaching
+});
+
+const nextConfig = withPWA({
     distDir: "build",
     reactStrictMode: true,
     swcMinify: true,
     images: { unoptimized: true }
-};
+});
 
 module.exports = nextConfig;
