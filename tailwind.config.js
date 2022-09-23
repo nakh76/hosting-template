f48d08c010508af58d8b8867780951d3d96bb51c
nakh76 <nakh76@gmail.com>
2022-09-23 13:56:03 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/pages/**/*.{js,jsx}"], // the pages are under ./src
+    theme: {
+        extend: {}
+    },
+    plugins: []
+};
